64ce4555536195548e4296b482a3de264c126beb
fuck <fuck@fuck.com>
2023-10-02 22:07:06 +0800

    remove garbages

--- src/render/tempCodeRunnerFile.rs
+++
@@ -1,5 +0,0 @@
-fn start_nobreak(&mut self) {
-        let (s, annotation) = self.decorator.decorate_code_start();
-        self.ann_stack.push(annotation);
-        self.add_inline_text(&s);
-    }
